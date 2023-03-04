55e980215e55ba68776d34a4507ea02dfde899ce
Dipeshtripathi13 <killredps@gmail.com>
2023-03-04 23:39:57 +0545

    Added To Database

---
+++ FutsalScheduleManagementWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using FutsalScheduleManagementWeb.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FutsalScheduleManagementWeb.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Schedule> Schedules { get; set; }
+    }
+}
